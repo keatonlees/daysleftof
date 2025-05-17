c8031d97cba55d7d629ea5332cbfe4d07f60da11
Keaton Lees <keatslees@yahoo.com>
2025-05-16 21:24:45 -0700

    Updated analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
         <AuthProvider>
           <NavBar />
           {children}
-          <Analytics />
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
