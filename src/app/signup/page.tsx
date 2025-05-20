723a7bef16067c8aa2a429814eb63fb001dd0e89
Keaton Lees <keatslees@yahoo.com>
2025-05-20 10:38:33 -0700

    Updated login/signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,114 +1,171 @@
 "use client";
 
 import { useAuth } from "@/contexts/AuthContext";
+import { Eye, EyeClosed } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function SignupPage() {
+export default function SignUpPage() {
+  const { user, signUp } = useAuth();
+  const router = useRouter();
+
+  const [error, setError] = useState("");
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   const [loading, setLoading] = useState(false);
-  const { signUp, user } = useAuth();
-  const router = useRouter();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    // Validation
     if (password !== confirmPassword) {
       setError("Passwords do not match");
       return;
     }
+    if (password.length <= 8) {
+      setError("Password must be longer than 8 characters");
+      return;
+    }
+    if (!password.match(/[a-z]/g)) {
+      setError("Password must contain lowercase letters");
+      return;
+    }
+    if (!password.match(/[A-Z]/g)) {
+      setError("Password must contain uppercase letters");
+      return;
+    }
+    if (!password.match(/[0-9]/g)) {
+      setError("Password must contain numbers");
+      return;
+    }
+    if (!password.match(/[!@#$%^&*]/g)) {
+      setError("Password must contain special characters");
+      return;
+    }
 
+    // Sign Up
     try {
-      setError("");
       setLoading(true);
+      setError("");
       await signUp(email, password);
-      router.push("/"); // Redirect to home page after successful signup
+      router.push("/");
     } catch (e: unknown) {
-      setError(
-        (e as Error).message || "Failed to create an account. Please try again."
-      );
-      console.error(e);
+      setError((e as Error).message || "Failed to create your account.");
+      console.log(e);
     } finally {
       setLoading(false);
     }
   };
 
-  // If user is already logged in, don't render the signup form
-  if (user) {
-    return null;
-  }
+  // if user is already logged in, don't render
+  if (user) return null;
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-base-200">
+    <div className="flex w-full h-screen justify-center items-center">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title text-2xl font-bold mb-4">Sign Up</h2>
+          <h1 className="card-title text-2xl font-bold">Sign Up</h1>
+
           {error && (
-            <div className="alert alert-error mb-4">
+            <div className="alert alert-error text-white">
               <span>{error}</span>
             </div>
           )}
-          <form onSubmit={handleSubmit}>
+
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            {/* Email Input */}
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
+              <h1 className="label">
+                <span className="label-text">
+                  Email<span className="text-error">*</span>
+                </span>
+              </h1>
+              <label className="input w-full rounded-xl">
+                <input
+                  type="email"
+                  placeholder="example@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="input input-bordered"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
             </div>
+
+            {/* Password Input */}
+            <div className="form-control">
+              <h1 className="label">
+                <span className="label-text">
+                  Password<span className="text-error">*</span>
+                </span>
+              </h1>
+              <label className="input w-full rounded-xl">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Mypassword123"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <span
+                  className="btn btn-sm btn-circle bg-transparent border-none"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <Eye size={16} /> : <EyeClosed size={16} />}
+                </span>
+              </label>
+            </div>
+
+            {/* Confirm Password Input */}
+            <div className="form-control">
+              <h1 className="label">
+                <span className="label-text">
+                  Confirm Password<span className="text-error">*</span>
+                </span>
+              </h1>
+              <label className="input w-full rounded-xl">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Mypassword123"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+                <span
+                  className="btn btn-sm btn-circle bg-transparent border-none"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? (
+                    <Eye size={16} />
+                  ) : (
+                    <EyeClosed size={16} />
+                  )}
+                </span>
+              </label>
+            </div>
+
+            {/* Submit Button */}
             <div className="form-control mt-4">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="input input-bordered"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-control mt-4">
-              <label className="label">
-                <span className="label-text">Confirm Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Confirm your password"
-                className="input input-bordered"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="form-control mt-6">
               <button
-                className="btn btn-primary"
                 type="submit"
+                className="btn btn-primary rounded-xl w-full"
                 disabled={loading}
               >
                 {loading ? "Creating Account..." : "Sign Up"}
               </button>
             </div>
           </form>
-          <div className="mt-4 text-center">
-            <p>
-              Already have an account?{" "}
-              <Link href="/login" className="link link-primary">
-                Login
-              </Link>
-            </p>
+
+          <div className="text-center mt-2 ">
+            <p>Already have an account?</p>
+            <Link href="/login" className="link">
+              Login
+            </Link>
           </div>
         </div>
       </div>
--- src/lib/Constants.ts
+++ src/lib/Constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL_DEV = "http://localhost:3000";
-export const BASE_URL_PROD = "https://daysleftof.vercel.app";
+export const BASE_URL_PROD = "https://www.daysleftof.com/";
